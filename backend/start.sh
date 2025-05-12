d52108275d924fb4aa1f62eaa7b226182a001764
Maxim Gladkov <maksim.gladkov@gmail.com>
2025-05-12 20:09:05 +0200

    chore: modify start.sh to run the web server in the background and wait for background processes

--- backend/start.sh
+++ backend/start.sh
@@ -69,4 +69,8 @@ PYTHON_CMD=$(command -v python3 || command -v python)
 
 node /proxy/index.js &
 
-WEBUI_SECRET_KEY="$WEBUI_SECRET_KEY" exec "$PYTHON_CMD" -m uvicorn open_webui.main:app --host "$HOST" --port "$PORT" --forwarded-allow-ips '*' --workers "${UVICORN_WORKERS:-1}"
+WEBUI_SECRET_KEY="$WEBUI_SECRET_KEY" exec "$PYTHON_CMD" -m uvicorn open_webui.main:app --host "$HOST" --port "$PORT" --forwarded-allow-ips '*' --workers "${UVICORN_WORKERS:-1}" &
+
+wait -n
+
+exit $?
