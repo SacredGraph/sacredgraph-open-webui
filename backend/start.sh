877532187fbc80bdb4e360313f07c0402f0a5824
Maxim Gladkov <maksim.gladkov@gmail.com>
2025-05-12 23:00:47 +0200

    chore: modify start.sh to run the web server in the foreground instead of as a background process

--- backend/start.sh
+++ backend/start.sh
@@ -80,7 +80,7 @@ trap cleanup SIGINT
 PYTHON_CMD=$(command -v python3 || command -v python)
 
 echo "Starting webui on port $PORT..."
-WEBUI_SECRET_KEY="$WEBUI_SECRET_KEY" exec "$PYTHON_CMD" -m uvicorn open_webui.main:app --host "$HOST" --port "$PORT" --forwarded-allow-ips '*' --workers "${UVICORN_WORKERS:-1}" &
+WEBUI_SECRET_KEY="$WEBUI_SECRET_KEY" exec "$PYTHON_CMD" -m uvicorn open_webui.main:app --host "$HOST" --port "$PORT" --forwarded-allow-ips '*' --workers "${UVICORN_WORKERS:-1}"
 server_pid1=$!
 
 echo "Starting proxy on port $PROXY_PORT..."
